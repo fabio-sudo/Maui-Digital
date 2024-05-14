3d96a3f99770184579032a7165e67792997b8c12
fabio-sudo <fabiotrevezane@gmail.com>
2024-05-14 07:29:23 -0300

    Atualizacao

---
+++ MauiBiometricPluginSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.Maui.Biometric;
+
+namespace MauiBiometricPluginSample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var result = await BiometricAuthenticationService.Default.AuthenticateAsync(new AuthenticationRequest()
+            {
+                Title = "Please authenticate to increment",
+                NegativeText = "Cancel authentication"
+            }, CancellationToken.None);
+
+            if (result.Status == BiometricResponseStatus.Success)
+            {
+                count++;
+
+                if (count == 1)
+                    CounterBtn.Text = $"Clicked {count} time";
+                else
+                    CounterBtn.Text = $"Clicked {count} times";
+
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+            else
+            {
+                await DisplayAlert("Nope", "Couldnt authenticate" , "OK");
+            }
+        }
+    }
+
+}
